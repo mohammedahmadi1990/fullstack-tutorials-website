862228b55c130bfc2db15d9400a05319e7b9d9d0
Med <mohammed.ahmadi1990@gmail.com>
2022-08-01 17:38:22 +0300

    Spring Up & Running
    SpringBoot is up and running correctly

---
+++ src/test/java/me/designdevelop/springboottutorial/SpringbootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package me.designdevelop.springboottutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
